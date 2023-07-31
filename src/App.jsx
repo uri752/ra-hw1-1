a41263018f18e6970671b619d08c2fead7ba07a1
uri752 <uri752blag@mail.ru>
2023-07-31 20:24:08 +0300

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { ShopItemFunc } from "./components/ShopItemFunc";
+import './components/main.css';
+
+const itemInfo = {
+    brand: 'Tiger of Sweden',
+    title: 'Leonard coat',
+    description: 'Minimalistic coat in cotton-blend',
+    descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+    price: 399,
+    currency: '£'
+};
+
+export const App = () => {
+    return (
+        <div className="container">
+            <div className="background-element">                
+            </div>
+            <div className="highlight-window">
+                <div className="highlight-overlay"></div>
+            </div>
+            <div className="window">
+                <ShopItemFunc item = {itemInfo}></ShopItemFunc>
+            </div>            
+        </div>        
+    )
+}
